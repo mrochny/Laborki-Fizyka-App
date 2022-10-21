aefa7e04628418d55bb3d309b35e5e1e94e093bf
mrochny <trollklop@gmail.com>
2022-10-21 23:26:58 +0200

    Show sample graph filled with random data

--- PhysicsVis/driver/DataVisualizer.py
+++ PhysicsVis/driver/DataVisualizer.py
@@ -3,7 +3,17 @@ import numpy as np
 
 class DataVisualizer:
 
+    data_x: np.ndarray
+    data_y: np.ndarray
 
-    @staticmethod
-    def showGraph():
-        print("That would show graph")
+
+    def showGraph(self):
+        fig, ax = plt.subplots()
+        ax.stem(self.data_x, self.data_y)
+        plt.show()
+
+    def debug_seed_random_data(self):
+        np.random.seed(4)
+        self.data_x = 1.0 + np.arange(9)
+        self.data_y = np.random.uniform(3, 9, len(self.data_x))
+
