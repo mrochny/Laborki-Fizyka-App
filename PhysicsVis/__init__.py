ff95347e7c6e1d73dfe05e7eef74e3df653ac888
mrochny <trollklop@gmail.com>
2022-10-23 15:07:49 +0200

    Add test data support

--- PhysicsVis/__init__.py
+++ PhysicsVis/__init__.py
@@ -4,6 +4,3 @@ from PhysicsVis.driver.DataVisualizer import DataVisualizer as dv
 class PhysicsVisualizer:
 
     math_engine: dv = dv()
-
-    def __init__(self):
-        self.math_engine.fetch_data("nothing.csv")
